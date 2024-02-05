74cfd314890cda0a0dc94f697a6b1d77bc740ab0
sync <85231933+syncwrld@users.noreply.github.com>
2024-02-05 17:33:51 -0300

    style & perf: remove unnecessary variable & chance method separation style

--- src/main/java/me/syncwrld/booter/database/IdentifiableRepository.java
+++ src/main/java/me/syncwrld/booter/database/IdentifiableRepository.java
@@ -4,17 +4,19 @@ import java.util.Map;
 
 public interface IdentifiableRepository {
   String getName();
+
   boolean hasMoreThanOneTable();
+
   void createTables();
+
   Map<String, Integer> getTableIDs();
 
   default String consolidate(String query) {
-    String consolidate = query;
     for (Map.Entry<String, Integer> entry : getTableIDs().entrySet()) {
       String placeholder = "$table_" + entry.getValue();
-      String tabelaName = entry.getKey();
-      consolidate = consolidate.replaceAll(placeholder, tabelaName);
+      String tableName = entry.getKey();
+      query = query.replaceAll(placeholder, tableName);
     }
-    return consolidate;
+    return query;
   }
 }
