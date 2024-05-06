547983bf4a1e5ef9f03cf8091d935f60e66b6337
sync. <85231933+syncwrld@users.noreply.github.com>
2024-05-06 15:45:13 -0300

    add: new serialization methods

---
+++ src/main/java/me/syncwrld/booter/minecraft/serialization/Serializer.java
@@ -0,0 +1,122 @@
+package me.syncwrld.booter.minecraft.serialization;
+
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.val;
+import me.syncwrld.booter.Constants;
+import org.bukkit.Bukkit;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+public class Serializer {
+
+  private final Gson gson = Constants.GSON;
+
+  public String serializeInventory(InventoryType inventoryType, Inventory inventory) {
+    ItemStack[] content = inventory.getContents();
+    Map<String, Object> serializationMap = new HashMap<>();
+    Map<Integer, String> items = new HashMap<>();
+
+    int index = 0;
+    for (ItemStack stack : content) {
+      Map<String, Object> itemData = new HashMap<>();
+
+      // Item properties
+      val enchantments = stack.getEnchantments();
+      int amount = stack.getAmount();
+      val itemMeta = stack.getItemMeta();
+      short durability = stack.getDurability();
+      val data = stack.getData();
+
+      itemData.put("enchantments", gson.toJson(enchantments));
+      itemData.put("amount", amount);
+      itemData.put("meta", gson.toJson(itemMeta, ItemMeta.class));
+      itemData.put("durability", durability);
+      itemData.put("data", gson.toJson(data, MaterialData.class));
+
+      items.put(index, gson.toJson(itemData));
+      itemData.clear();
+
+      index++;
+    }
+
+    serializationMap.put("type", gson.toJson(inventoryType));
+    serializationMap.put("content", items);
+
+    return gson.toJson(serializationMap);
+  }
+
+  public ItemStack[] deserializeInventoryContent(String serializedInventory) {
+    Map<String, Object> deserializationMap = gson.fromJson(serializedInventory, HashMap.class);
+
+    InventoryType inventoryType =
+        gson.fromJson((String) deserializationMap.get("type"), InventoryType.class);
+    Map<Integer, String> itemsMap =
+        gson.fromJson(gson.toJson(deserializationMap.get("content")), HashMap.class);
+
+    ItemStack[] contents = new ItemStack[inventoryType.getDefaultSize()];
+
+    for (Map.Entry<Integer, String> entry : itemsMap.entrySet()) {
+      int index = entry.getKey();
+      String itemDataJson = entry.getValue();
+
+      Map<String, Object> itemData = gson.fromJson(itemDataJson, HashMap.class);
+
+      Map<Enchantment, Integer> enchantments =
+          gson.fromJson((String) itemData.get("enchantments"), HashMap.class);
+      int amount = (int) itemData.get("amount");
+      ItemMeta itemMeta = gson.fromJson((String) itemData.get("meta"), ItemMeta.class);
+      short durability = ((Number) itemData.get("durability")).shortValue();
+      MaterialData data = gson.fromJson((String) itemData.get("data"), MaterialData.class);
+
+      ItemStack itemStack = new ItemStack(data.getItemType(), amount, durability);
+      itemStack.setItemMeta(itemMeta);
+      itemStack.setData(data);
+      itemStack.addUnsafeEnchantments(enchantments);
+
+      contents[index] = itemStack;
+    }
+
+    return contents;
+  }
+
+  public Inventory deserializeInventory(String serializedInventory) {
+    Map<String, Object> deserializationMap = gson.fromJson(serializedInventory, HashMap.class);
+
+    InventoryType inventoryType =
+        gson.fromJson((String) deserializationMap.get("type"), InventoryType.class);
+    Map<Integer, String> itemsMap =
+        gson.fromJson(gson.toJson(deserializationMap.get("content")), HashMap.class);
+
+    Inventory inventory = Bukkit.createInventory(null, inventoryType);
+
+    for (Map.Entry<Integer, String> entry : itemsMap.entrySet()) {
+      int index = entry.getKey();
+      String itemDataJson = entry.getValue();
+
+      Map<String, Object> itemData = gson.fromJson(itemDataJson, HashMap.class);
+
+      Map<Enchantment, Integer> enchantments =
+          gson.fromJson((String) itemData.get("enchantments"), HashMap.class);
+      int amount = (int) itemData.get("amount");
+      ItemMeta itemMeta = gson.fromJson((String) itemData.get("meta"), ItemMeta.class);
+      short durability = ((Number) itemData.get("durability")).shortValue();
+      MaterialData data = gson.fromJson((String) itemData.get("data"), MaterialData.class);
+
+      ItemStack itemStack = new ItemStack(data.getItemType(), amount, durability);
+      itemStack.setItemMeta(itemMeta);
+      itemStack.setData(data);
+      itemStack.addUnsafeEnchantments(enchantments);
+
+      inventory.setItem(index, itemStack);
+    }
+
+    return inventory;
+  }
+}
