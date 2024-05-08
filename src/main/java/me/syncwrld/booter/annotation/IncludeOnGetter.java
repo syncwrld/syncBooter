6af163e9b6a4d5f0d2bd608aba789d787e1a379f
sync. <85231933+syncwrld@users.noreply.github.com>
2024-05-08 13:33:25 -0300

    add: annotation to solve included fields on lombok getter

---
+++ src/main/java/me/syncwrld/booter/annotation/IncludeOnGetter.java
@@ -0,0 +1,10 @@
+package me.syncwrld.booter.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface IncludeOnGetter {}
