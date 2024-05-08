1316ee9df612d80724f81b70128f35d2b7873bee
sync. <85231933+syncwrld@users.noreply.github.com>
2024-05-08 13:34:59 -0300

    add: now booter load as a spigot plugin

--- src/main/java/me/syncwrld/booter/minecraft/SpigotLoader.java
+++ src/main/java/me/syncwrld/booter/minecraft/SpigotLoader.java
@@ -1,5 +1,19 @@
 package me.syncwrld.booter.minecraft;
 
+import me.syncwrld.booter.minecraft.loader.BukkitPlugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SpigotLoader extends JavaPlugin {}
+public class SpigotLoader extends BukkitPlugin {
+    @Override
+    protected void whenLoad() {
+        this.setPrefix("&b[syncBooter] ");
+    }
+
+    @Override
+    protected void whenEnable() {
+        this.log("&eThis plugin will not execute any action on your server, he is only a development library.");
+    }
+
+    @Override
+    protected void whenDisable() {}
+}
