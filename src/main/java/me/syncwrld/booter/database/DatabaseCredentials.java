011d963bed8c4c733533c61ab0d55759256d0a29
sync. <85231933+syncwrld@users.noreply.github.com>
2024-05-08 13:34:33 -0300

    add: part of new connection code

--- src/main/java/me/syncwrld/booter/database/DatabaseCredentials.java
+++ src/main/java/me/syncwrld/booter/database/DatabaseCredentials.java
@@ -1,3 +1,22 @@
 package me.syncwrld.booter.database;
 
-public class DatabaseCredentials {}
+import lombok.Data;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
+@Data
+public class DatabaseCredentials {
+  private final String username;
+  private final String password;
+  private final String host;
+  private final String database;
+
+  public static DatabaseCredentials withConfiguration(FileConfiguration configuration) {
+    final ConfigurationSection databaseSection = configuration.getConfigurationSection("database");
+    return new DatabaseCredentials(
+        databaseSection.getString("username"),
+        databaseSection.getString("password"),
+        databaseSection.getString("host"),
+        databaseSection.getString("database"));
+  }
+}
