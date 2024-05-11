871aafa2c27994eaf1ef320d0341d9773a644b3b
sync. <85231933+syncwrld@users.noreply.github.com>
2024-05-11 07:41:50 -0300

    add: new methods to parse arrays and save others

--- src/main/java/me/syncwrld/booter/common/tool/JsonParser.java
+++ src/main/java/me/syncwrld/booter/common/tool/JsonParser.java
@@ -1,5 +1,6 @@
 package me.syncwrld.booter.common.tool;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 import java.io.*;
@@ -28,4 +29,41 @@ public class JsonParser {
       return Constants.GSON.fromJson(jsonReader, JsonObject.class);
     }
   }
+
+  public JsonArray getArray(File file) {
+    if (!(file.exists())) {
+      Booter.getLogger().info("File not found: " + file.getAbsolutePath());
+    }
+    try (Reader reader = new FileReader(file)) {
+      JsonReader jsonReader = new JsonReader(reader);
+      return Constants.GSON.fromJson(jsonReader, JsonArray.class);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public JsonArray getArray(InputStream inputStream) {
+    InputStreamReader reader = new InputStreamReader(inputStream);
+    try (Reader readerObj = new BufferedReader(reader)) {
+      JsonReader jsonReader = new JsonReader(readerObj);
+      return Constants.GSON.fromJson(jsonReader, JsonArray.class);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @SneakyThrows
+  public void saveObject(File file, JsonObject object) {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+      Constants.GSON.toJson(object, writer);
+    }
+  }
+
+  @SneakyThrows
+  public void saveArray(File file, JsonArray array) {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+      Constants.GSON.toJson(array, writer);
+    }
+  }
+  
 }
