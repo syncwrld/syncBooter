d51d29c02ee10cf6da904664354d5a77aa7c520e
sync. <85231933+syncwrld@users.noreply.github.com>
2024-05-17 14:47:33 -0300

    add: new BukkitDatabaseCredentials to bukkit implementions

---
+++ src/main/java/me/syncwrld/booter/database/BukkitDatabaseCredentials.java
@@ -0,0 +1,37 @@
+package me.syncwrld.booter.database;
+
+import lombok.Data;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
+@Data
+public class BukkitDatabaseCredentials {
+  private final String username;
+  private final String password;
+  private final String host;
+  private final String database;
+
+  /*
+  Example configuration:
+  # Database #
+  database:
+    type: 'SQLite' # MySQL or SQLite
+    # SQLite #
+    sqlite-file: 'storage/database.db'
+    # MySQL #
+   host: 'localhost'
+   database: 's1_booter'
+   username: 'root'
+   password: ''
+   */
+
+  public static BukkitDatabaseCredentials withConfiguration(FileConfiguration configuration) {
+    final ConfigurationSection databaseSection = configuration.getConfigurationSection("database");
+    return new BukkitDatabaseCredentials(
+        databaseSection.getString("username"),
+        databaseSection.getString("password"),
+        databaseSection.getString("host"),
+        databaseSection.getString("database"));
+  }
+
+}
